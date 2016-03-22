54605e20e34822d69685cb26b829820e1be33f1e
Mathieu Schroeter <mathieu@schroetersa.ch>
2016-03-22 18:18:28 +0100

    Cosmetic: remove trailing whitespaces, fix coding style

--- bin/mai-chai-init.js
+++ bin/mai-chai-init.js
@@ -9,9 +9,11 @@ var mkdirSync = function (path) {
   try {
     fs.mkdirSync (path);
   } catch (e) {
-    if (e.code != 'EEXIST') throw e;
+    if (e.code !== 'EEXIST') {
+      throw e;
+    }
   }
-}
+};
 
 var cwd = process.cwd ().replace (/\\/g, '/');
 var suffix = '/node_modules/mai-chai';
@@ -20,15 +22,15 @@ if (cwd.endsWith (suffix)) {
   var root = cwd.substr (0, cwd.length - suffix.length);
   var test = path.join (root, 'test');
   var files = fs.readdirSync (path.join (cwd, 'templates'));
-  
+
   mkdirSync (test);
   mkdirSync (path.join (root, 'src'));
   mkdirSync (path.join (root, 'src.test'));
-  
+
   files.forEach (function (file) {
     var data = fs.readFileSync (path.join (cwd, 'templates', file));
     if (file.substr (0, 2) === '--') {
-      var name = file.substr (2, file.length - 2); 
+      var name = file.substr (2, file.length - 2);
       fs.writeFileSync (path.join (root, name), data);
     } else {
       fs.writeFileSync (path.join (test, file), data);

8cf44a69b067f262d9cd13e4477de9e2776ca75c
Mathieu Schroeter <mathieu@schroetersa.ch>
2016-03-22 18:21:44 +0100

    Use the right line terminator accordingly to the platform

--- bin/mai-chai-init.js
+++ bin/mai-chai-init.js
@@ -27,13 +27,20 @@ if (cwd.endsWith (suffix)) {
   mkdirSync (path.join (root, 'src'));
   mkdirSync (path.join (root, 'src.test'));
 
+  var regex = /\r\n/g;
+  var newline = '\n';
+  if (process.platform === 'win32') {
+    regex = /[^\r]\n/g;
+    newline = '\r\n';
+  }
+
   files.forEach (function (file) {
     var data = fs.readFileSync (path.join (cwd, 'templates', file));
     if (file.substr (0, 2) === '--') {
       var name = file.substr (2, file.length - 2);
-      fs.writeFileSync (path.join (root, name), data);
+      fs.writeFileSync (path.join (root, name), data.replace (regex, newline));
     } else {
-      fs.writeFileSync (path.join (test, file), data);
+      fs.writeFileSync (path.join (test, file), data.replace (regex, newline));
     }
   });
 }

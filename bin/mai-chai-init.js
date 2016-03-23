4b868ad5d35c22f2daa2ee3979bfc3a8cab4a367
Mathieu Schroeter <mathieu@schroetersa.ch>
2016-03-23 08:04:27 +0100

    Convert data to a string then apply the regex

--- bin/mai-chai-init.js
+++ bin/mai-chai-init.js
@@ -38,9 +38,9 @@ if (cwd.endsWith (suffix)) {
     var data = fs.readFileSync (path.join (cwd, 'templates', file));
     if (file.substr (0, 2) === '--') {
       var name = file.substr (2, file.length - 2);
-      fs.writeFileSync (path.join (root, name), data.replace (regex, newline));
+      fs.writeFileSync (path.join (root, name), data.toString ().replace (regex, newline));
     } else {
-      fs.writeFileSync (path.join (test, file), data.replace (regex, newline));
+      fs.writeFileSync (path.join (test, file), data.toString ().replace (regex, newline));
     }
   });
 }
